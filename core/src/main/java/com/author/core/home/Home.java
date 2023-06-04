c6cfb0b10efa9be278aa8aeb3303fea00ae3ba5b
Osiris Team <osiris_support@pm.me>
2023-06-04 12:33:05 +0200

    update example

--- core/src/main/java/com/author/core/home/Home.java
+++ core/src/main/java/com/author/core/home/Home.java
@@ -6,7 +6,9 @@ import com.osiris.desku.ui.Component;
 import com.osiris.desku.ui.display.RTable;
 import com.osiris.desku.ui.display.Table;
 import com.osiris.desku.ui.display.Text;
+import com.osiris.desku.ui.layout.PageLayout;
 import com.osiris.desku.ui.layout.Vertical;
+import com.osiris.jlib.logger.AL;
 
 import java.io.IOException;
 import java.util.ArrayList;
@@ -31,14 +33,11 @@ public class Home extends Route {
     @Override
     public Component<?> loadContent() {
         Vertical ly = vertical().childGap(true).padding(true);
-        ly.add(new Text(App.name).sizeXL().selfCenter());
-        ly.add(new Text("Desku template app to get started quickly.").selfCenter());
-
         ly.horizontalCL().size("100%", "70vh").childCenter()
-                .add(text("Build Desktop Apps with Java, HTML and CSS today!")
-                        .sizeXXL().selfCenter()
-                        .size("40vh", "20vh").bold())
-                .add(image(this.getClass(), "/images/pc.png").width("30vw").selfCenter());
+            .add(text("Build Desktop Apps with Java, HTML and CSS today!")
+                .sizeXXL().selfCenter()
+                .size("40vh", "20vh").bold())
+            .add(image(this.getClass(), "/images/pc.png").width("30vw").selfCenter());
         ly.add(text("I am clickable text! Click me!").selfCenter().onClick(event -> {
             event.comp.set("Thank you for clicking!");
         }));
@@ -46,6 +45,7 @@ public class Home extends Route {
         //
         // Async
         //
+        ly.add(text("Async").sizeXXL());
         ly.add(text("Asynchronously update a component: Loading...").padding(true).later(txt -> {
             try {
                 for (int i = 1; i <= 100; i++) {
@@ -53,7 +53,7 @@ public class Home extends Route {
                     txt.set("Asynchronously update a component: Loading... " + i + "%");
                 }
             } catch (Exception e) {
-                e.printStackTrace();
+                AL.warn(e);
             }
         }));
         ly.add(vertical().size("300px", "100px").laterWithOverlay((comp, overlay) -> {
@@ -64,67 +64,135 @@ public class Home extends Route {
                 txt.set("Finished after 10 seconds!");
                 comp.putStyle("background", "#32a852");
             } catch (Exception e) {
-                e.printStackTrace();
+                AL.warn(e);
             }
         }));
 
         //
-        // Layouts
+        // Layouts and Text
         //
+        ly.add(text("Simple layouts and text").sizeXXL());
         ly.verticalCL()
-                .add(text("Child vertical layout. Items: "))
-                .add(text("XSmall").sizeXS())
-                .add(text("Small").sizeS())
-                .add(text("Medium").sizeM())
-                .add(text("Large").sizeL())
-                .add(text("XLarge").sizeXL())
-                .add(text("XLarge").sizeXL())
-                .add(text("XXLarge").sizeXXL())
-                .add(text("XXXLarge").sizeXXXL());
+            .add(text("Vertical child layout. Items: "))
+            .add(text("XSmall").sizeXS())
+            .add(text("Small").sizeS())
+            .add(text("Medium").sizeM())
+            .add(text("Large").sizeL())
+            .add(text("XLarge").sizeXL())
+            .add(text("XLarge").sizeXL())
+            .add(text("XXLarge").sizeXXL())
+            .add(text("XXXLarge").sizeXXXL());
 
         ly.horizontalCL()
-                .add(text("Child horizontal layout. Items: "))
-                .add(text("XSmall").sizeXS())
-                .add(text("Small").sizeS())
-                .add(text("Medium").sizeM())
-                .add(text("Large").sizeL())
-                .add(text("XLarge").sizeXL())
-                .add(text("XXLarge").sizeXXL())
-                .add(text("XXXLarge").sizeXXXL());
+            .add(text("Horizontal child layout. Items: "))
+            .add(text("XSmall").sizeXS())
+            .add(text("Small").sizeS())
+            .add(text("Medium").sizeM())
+            .add(text("Large").sizeL())
+            .add(text("XLarge").sizeXL())
+            .add(text("XXLarge").sizeXXL())
+            .add(text("XXXLarge").sizeXXXL());
+        // Smart, mobile friendly layout
+        ly.add(text("Smart, mobile friendly layout").sizeXXL());
+        ly.add(smartlayout().add(
+            text("1").putStyle("background-color", "var(--color-primary-50)"),
+            text("2").putStyle("background-color", "var(--color-primary-50)"),
+            text("3").putStyle("background-color", "var(--color-primary-50)"),
+            text("4").putStyle("background-color", "var(--color-primary-50)"),
+            text("5").putStyle("background-color", "var(--color-primary-50)")));
+        // Scroll layout
+        ly.add(text("Scroll layout").sizeXXL());
+        ly.add(
+            vertical().childGap(true).scrollable(true, "100%", "100px", // Size for scroll layout
+                    "100%", "5px") // Min sizes for children
+                .onScroll(e -> {
+                    System.out.println("SCROLL: "+e.rawJSMessage);
+                })
+        );
+        for (int i = 0; i < 20; i++) {
+            ly.lastChild().add(vertical().putStyle("background-color", "gray"));
+        }
+        // Page layout
+        ly.add(text("Page layout").sizeXXL());
+        int[] data = new int[10];
+        for (int i = 0; i < 10; i++) {
+            data[i] = i;
+        }
+        ly.add(
+            new PageLayout().childGap(true).setDataProvider(0, 3, (details) -> {
+                List<Component<?>> comps = new ArrayList<>();
+                for (int i = Math.max(details.iStart, 0); i < Math.min(details.iEnd, data.length - 1); i++) {
+                    comps.add(text("Index: "+data[i]).width("100%").putStyle("background-color", "lightgray"));
+                }
+                try{Thread.sleep(1000);} catch (Exception e) {}
+                return comps;
+            })
+        );
+
+        //
+        // Navigate between routes
+        //
+        ly.add(text("Navigate between routes").sizeXXL());
+        //ly.add(router().set(About.class).add(text("Go to About page!")));
+        ly.add(router().set("/about").add(text("Go to About page!")));
+        ly.add(router().set("https://google.com").add(text("Go to Google page!")));
 
         //
         // Overlays
         //
+        ly.add(text("Overlays").sizeXXL());
         ly.add(overlay(null).add(text("Overlay over the page")));
         ly.verticalCL().putStyle("background-color", "blue").size("100px", "100px").add(
-                overlay(ly.lastChild()).putStyle("background-color", "red")
-                        .add(text("Overlay over parent.")));
+            overlay(ly.lastChild()).putStyle("background-color", "red")
+                .add(text("Overlay over another component.")));
 
         //
         // Inputs
         //
+        ly.add(text("Inputs").sizeXXL());
         AtomicInteger i = new AtomicInteger();
         ly.horizontalCL().childGap(true).width("100%")
-                .add(button("This is a button!").grow(1).onClick(e -> {
-            String s = "Clicked " + i.incrementAndGet() + " times";
-            System.out.println(s);
-            e.comp.text.set(s);
-        }), button("This is a button!").grow(1).onClick(e -> {
-            String s = "Clicked " + i.incrementAndGet() + " times";
-            System.out.println(s);
-            e.comp.text.set(s);
-        }), button("This is a button!").grow(1).onClick(e -> {
-            String s = "Clicked " + i.incrementAndGet() + " times";
-            System.out.println(s);
-            e.comp.text.set(s);
+            .add(button("Click me!").grow(1).onClick(e -> {
+                String s = "Clicked " + i.incrementAndGet() + " times";
+                System.out.println(s);
+                e.comp.text.set(s);
+            }), button("Click me!").grow(1).onClick(e -> {
+                String s = "Clicked " + i.incrementAndGet() + " times";
+                System.out.println(s);
+                e.comp.text.set(s);
+            }), button("Click me!").grow(1).onClick(e -> {
+                String s = "Clicked " + i.incrementAndGet() + " times";
+                System.out.println(s);
+                e.comp.text.set(s);
+            }));
+        // Button variants
+        ly.horizontalCL().childGap(true).add(button("Primary").primary(), button("Secondary").secondary(), button("Success").success(),
+            button("Danger").danger(), button("Warning").warning(), button("Info").info(),
+            button("Light").light(), button("Dark").dark());
+        // Fields
+        ly.add(textfield("Text field label", "Def").onValueChange(e -> {
+            System.out.println("Input of textfield changed: "+e.value+" before: "+e.valueBefore);
+        }));
+        ly.add(passwordfield("Password field label").onValueChange(e -> {
+            System.out.println("Input of passwordfield changed: "+e.value+" before: "+e.valueBefore);
+        }));
+        ly.add(checkbox("Checkbox label").onValueChange(e -> {
+            System.out.println("Input of checkbox changed: "+e.value+" before: "+e.valueBefore);
+        }));
+        ly.add(colorpicker("Color picker label").onValueChange(e -> {
+            System.out.println("Input of colorpicker changed: "+e.value+" before: "+e.valueBefore);
+        }));
+        ly.add(slider("Slider label").onValueChange(e -> {
+            System.out.println("Input of slider changed: "+e.value+" before: "+e.valueBefore);
         }));
 
         //
         // Tables
         //
+        ly.add(text("Tables").sizeXXL());
         ly.add(new Table().headers("Header 1", "Header 2")
-                .row("Data 1", "Data 2aaaaa")
-                .row("Data 3aa", "Data 4"));
+            .row("Data 1", "Data 2aaaaa")
+            .row("Data 3aa", "Data 4"));
 
         // Tables via reflection
         class Person {
@@ -148,6 +216,16 @@ public class Home extends Route {
             throw new RuntimeException(e);
         }
 
+        //
+        // Loading animations
+        //
+        ly.add(text("Loading animations").sizeXXL());
+        ly.horizontalCL().childGap(true).add(spinner().primary(), spinner().secondary(), spinner().success(),
+            spinner().danger(), spinner().warning(), spinner().info(),
+            spinner().light(), spinner().dark());
+        ly.horizontalCL().childGap(true).add(spinner().typeGrow().primary(), spinner().typeGrow().secondary(), spinner().typeGrow().success(),
+            spinner().typeGrow().danger(), spinner().typeGrow().warning(), spinner().typeGrow().info(),
+            spinner().typeGrow().light(), spinner().typeGrow().dark());
 
         return ly;
     }
